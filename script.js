b654372bcb80913a39c3f4144fb3918e5553bff2
Abdulus von Fibulus <shiugamingyt123@gmail.com>
2025-04-10 17:09:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-let cart = [];
-let totalPrice = 0;
-
-const toRupiah = (number) => {
-    let rupiah = '';
-    const numberStr = number.toString();
-    const numberStrRev = numberStr.split('').reverse().join('');
-    for (let i = 0; i < numberStrRev.length; i++) {
-        if (i % 3 === 0) {
-            rupiah += numberStrRev[i] + '.';
-        } else {
-            rupiah += numberStrRev[i];
-        }
-    }
-    const rupiahRev = rupiah.split('').reverse().join('');
-    return 'Rp. ' + rupiahRev.substring(0, rupiahRev.length - 1);
+function showDetails(itemName, itemDescription) {
+    alert(`${itemName}\n${itemDescription}`);
 }
-
-function addToCart(productName, productPrice) {
-    cart.push({ name: productName, price: productPrice });
